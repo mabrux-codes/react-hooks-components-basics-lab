db42a3725e3f8c9be24b1f314574b6bc4bb8ac96
Abel Lukwaro <abelmabruke@gmail.com>
2025-04-10 23:52:33 +0300

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,25 @@
+// components/App.js
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+    <div className="App">
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </nav>
+
+      <section id="home">
+        <h2>Home Section</h2>
+        <p>Welcome to the home section.</p>
+      </section>
+
+      <section id="about">
+        <h2>About Section</h2>
+        <p>This is the about section.</p>
+      </section>
     </div>
   );
 }
